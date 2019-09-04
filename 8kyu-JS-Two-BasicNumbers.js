9a6e2b70c7f9637959b1d6bf888cebbbd3c348ec
Lrop <Lagropner04@gmail.com>
2019-09-04 11:08:03 -0600

    cw

---
+++ 8kyu-JS-Two-BasicNumbers.js
@@ -0,0 +1,34 @@
+var v1=50;
+var v2=100;
+var v3=150;
+var v4=200; 
+var v5=2; 
+var v6=250;
+
+
+function equal1(){
+  var a=v1   
+  var b=v1   
+  return a+b;
+}
+//Please refer to the example above to complete the following functions
+function equal2(){
+  var a=v3   //set number value to a
+  var b=v1   //set number value to b
+  return a-b;
+}
+function equal3(){
+  var a=v1   //set number value to a
+  var b=v5   //set number value to b
+  return a*b;
+}
+function equal4(){
+  var a=v4   //set number value to a
+  var b=v5   //set number value to b
+  return a/b;
+}
+function equal5(){
+  var a=v2   //set number value to a
+  var b=v4   //set number value to b
+  return a%b;
+}
